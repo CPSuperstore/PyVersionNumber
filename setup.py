8e5371613f554ca0a5fc736e585fe44c4d1d1897
Christopher <cpsuperstoreinc@gmail.com>
2021-09-11 10:04:53 -0400

    Added long description to setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,36 @@
-from distutils.core import setup
-setup(
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
     name='PyVersionNumber',
     packages=['PyVersionNumber'],
-    version='1.0.0',
+    version='1.0.1',
     license='MIT',
-    description='A small library for handling and comparing Semantic Version Numbers ',
+    description='A small library for handling and comparing Semantic Version Numbers',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='CPSuperstore',
     author_email='cpsuperstoreinc@gmail.com',
     url='https://github.com/CPSuperstore/PyVersionNumber',
-    download_url='https://github.com/CPSuperstore/PyVersionNumber/archive/refs/tags/v1.0.0.tar.gz',
+    project_urls={
+        "Bug Tracker": "https://github.com/CPSuperstore/PyVersionNumber/issues",
+    },
+    download_url='https://github.com/CPSuperstore/PyVersionNumber/archive/refs/tags/v1.0.1.tar.gz',
     keywords=['SEMANTIC', 'VERSION', 'NUMBER', 'VERSIONING', 'MAJOR', 'MINOR', 'PATCH'],
     install_requires=[],
     classifiers=[
-        'Development Status :: "5 - Production/Stable',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
+        'Topic :: Software Development :: Documentation',
+        'Topic :: Documentation',
         'Topic :: Software Development :: Version Control',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Natural Language :: English'
     ],
 )
